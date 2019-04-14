3ec2eaca4fc35b06ecc686b2e7538e56c3f066d0
Dave <Dave>
2019-04-14 18:34:00 +1000

    Import Firebase core

---
+++ dporject/dporject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dporject
+//
+//  Created by THE DUY NGUYEN on 14/4/19.
+//  Copyright © 2019 THE DUY NGUYEN. All rights reserved.
+//
+
+import UIKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
