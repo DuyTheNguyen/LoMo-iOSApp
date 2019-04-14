366d3ab6f2e2d6bda4257bece21aa3eed83ab03c
Dave <Dave>
2019-04-14 19:42:14 +1000

    Testing with real time database

--- dporject/dporject/ViewController.swift
+++ dporject/dporject/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    
+    var dataRef  : DatabaseReference!
+    var emailRef: DatabaseReference!
+    
+    
+    @IBAction func nameChangeTapped(_ sender: Any) {
+          emailRef.setValue(emailTextField.text)
+    }
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        dataRef = Database.database().reference()
+        emailRef = dataRef.child("someid/name")
+        
+        
+        
     }
 
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        emailRef!.observe(DataEventType.value, with: { (snapshot) in
+            self.emailLabel.text = snapshot.value as! String
+            
+        })
+    }
+    
 }
 
