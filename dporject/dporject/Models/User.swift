c66e40fd960f6bd42b218624ba9f1951219480bd
Dave <duy27295@gmail.com>
2019-04-24 19:21:53 +1000

    add user model

---
+++ dporject/dporject/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  dporject
+//
+//  Created by THE DUY NGUYEN on 24/4/19.
+//  Copyright © 2019 THE DUY NGUYEN. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let uid: String
+    let email: String
+    let password: String
+    let displayName: String
+    let photoURL: String
+}
