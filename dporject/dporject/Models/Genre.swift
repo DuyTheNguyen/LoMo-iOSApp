5d206a2bedc15303fb2e8a72028e35940a95a0b1
Dave <duy27295@gmail.com>
2019-05-12 16:54:40 +1000

    create Genre Model

---
+++ dporject/dporject/Models/Genre.swift
@@ -0,0 +1,14 @@
+//
+//  Genre.swift
+//  dporject
+//
+//  Created by THE DUY NGUYEN on 12/5/19.
+//  Copyright © 2019 THE DUY NGUYEN. All rights reserved.
+//
+
+import Foundation
+
+struct Genre: Decodable{
+    let name: String?
+    let image: String?
+}
