befa69290880267cd7f90eed68270acf3abc8d17
Dave <duy27295@gmail.com>
2019-05-15 20:16:08 +1000

    create comment model

---
+++ dporject/dporject/Models/Comment.swift
@@ -0,0 +1,16 @@
+//
+//  Comment.swift
+//  dporject
+//
+//  Created by THE DUY NGUYEN on 15/5/19.
+//  Copyright © 2019 THE DUY NGUYEN. All rights reserved.
+//
+
+import Foundation
+
+struct Comment: Decodable{
+    let name: String?
+    let image: String?
+    let content: String?
+    let timestamp: String?
+}
