a459d8f2939a5a03055de84683d82dbf0283394b
Dave <duy27295@gmail.com>
2019-05-15 19:50:10 +1000

    add comment modal and its related files

--- dporject/dporject/Classes/Extension.swift
+++ dporject/dporject/Classes/Extension.swift
@@ -39,6 +39,8 @@ extension UIView{
     }
 }
 
+
+
 /************************************************
  *************** End: UIView ********************
  ************************************************/
@@ -147,3 +149,36 @@ extension UILabel{
 /************************************************
  *************** End: UILabel *******************
  ************************************************/
+
+
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
