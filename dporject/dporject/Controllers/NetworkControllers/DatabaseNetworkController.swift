973b8748e4a888bc9979b9080808a87e134b34c5
Dave <duy27295@gmail.com>
2019-05-16 14:02:22 +1000

    update data network controller

--- dporject/dporject/Controllers/NetworkControllers/DatabaseNetworkController.swift
+++ dporject/dporject/Controllers/NetworkControllers/DatabaseNetworkController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import FirebaseDatabase
 
+
 class DatabaseNetworkController{
     
     private var rootReference: DatabaseReference!
@@ -41,6 +42,30 @@ class DatabaseNetworkController{
        
     }
     
+    //Observe the data in the database
+    func observeDatabase(path:String){
+        guard let rootReference = rootReference else{
+            print("Something went wrong with root reference")
+            return
+        }
+        rootReference.child(path).observe(.value) { (snapshot) in
+            guard let values = snapshot.value as? [String:AnyObject] else {
+                fatalError("Could not load list of comments")
+            }
+            var comments = [Comment]()
+            for (_,value) in values{
+                let comment = Comment(snapshot: value)
+                comments.append(comment)
+            }
+            
+        }
+    }
+    
+    //Remove all observation
+    func removeObserveDatabase(path: String){
+        
+    }
+    
     //Get genres, movies and comment
     func getListOfObjectsFrom(path:String, withDataType: String){
         guard let rootReference = rootReference else{
@@ -50,39 +75,41 @@ class DatabaseNetworkController{
         
         //Get the list of object
         rootReference.child(path).observeSingleEvent(of: .value, with: { (snapshot) in
-            
-            
             if let values = snapshot.value as? [String:AnyObject] {
+                //Begin: Switch
                 switch withDataType {
-                case "Movie":
-                    var movies = [Movie]()
-                    for (_,value) in values{
-                        let movie = Movie(id: value["id"] as? String,
-                                          name: value["name"] as? String,
-                                          rating: value["rating"] as? String,
-                                          year: value["year"] as? String,
-                                          image: value["image"] as? String,
-                                          genre: value["genre"] as? String,
-                                          description: value["description"] as? String,
-                                          director: value["director"] as? String)
-                        movies.append(movie)
-                    }
-                    self.delegate?.didReceivedListOfMovies(movies: movies)
-                case "Genre":
-                    var genres = [Genre]()
-                    for(_, value) in values{
-                        let genre = Genre(name: value["name"] as? String,
-                                          image: value["image"] as? String)
-                        genres.append(genre)
-                    }
-                    self.delegate?.didReceivedListOfGenres(genres: genres)
-                default:
-                    print("Something went wrong in the switch case")
-                    print("Path: \(rootReference.child(path))")
-                    print("Data type: \(withDataType)")
+                    case "Movie":
+                        var movies = [Movie]()
+                        
+                        for (_,value) in values{
+                            let movie = Movie(id: value["id"] as? String,
+                                              name: value["name"] as? String,
+                                              rating: value["rating"] as? String,
+                                              year: value["year"] as? String,
+                                              image: value["image"] as? String,
+                                              genre: value["genre"] as? String,
+                                              description: value["description"] as? String,
+                                              director: value["director"] as? String)
+                            movies.append(movie)
+                        
+                        }
+                        
+                        self.delegate?.didReceivedListOfMovies(movies: movies)
+                    case "Genre":
+                        var genres = [Genre]()
+                        for(_, value) in values{
+                            let genre = Genre(name: value["name"] as? String,
+                                              image: value["image"] as? String)
+                            genres.append(genre)
+                        }
+                        self.delegate?.didReceivedListOfGenres(genres: genres)
+                    default:
+                        print("Something went wrong in the switch case")
+                        print("Path: \(rootReference.child(path))")
+                        print("Data type: \(withDataType)")
                 }
-                
-                
+                //End: Switch
+
             }else{
                 print("Cannot get values")
                 print("Value exist? : \(snapshot.exists())")
--- dporject/dporject/Models/Comment.swift
+++ dporject/dporject/Models/Comment.swift
@@ -8,11 +8,29 @@
 
 import Foundation
 
-struct Comment: Decodable{
-    let commentId: String
-    let userId: String
-    let userName: String
-    let image: String
-    let content: String
-    let timestamp: String
+struct Comment{
+    var commentId: String!
+    var userId: String!
+    var userName: String!
+    var image: String!
+    var content: String!
+    var timestamp: String!
+    
+    init(commentId: String, userId: String, userName: String, image: String, content: String, timestamp: String){
+        self.commentId = commentId
+        self.userId = userId
+        self.image = image
+        self.content = content
+        self.timestamp = timestamp
+        self.userName = userName
+    }
+    
+    init(snapshot: AnyObject){
+        self.commentId = snapshot["commentId"] as? String
+        self.userId = snapshot["userId"] as? String
+        self.image = snapshot["image"] as? String
+        self.content = snapshot["content"] as? String
+        self.timestamp = snapshot["timestamp"] as? String
+        self.userName = snapshot["userName"] as? String
+    }
 }
