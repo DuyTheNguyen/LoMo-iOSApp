ae7e71d451f6dc7b973a89ebc16ee9b9b35bf862
Dave <duy27295@gmail.com>
2019-05-16 12:43:34 +1000

    add comment list collection view cell

---
+++ dporject/dporject/View/Movie/CommentListCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CommentListCollectionViewCell.swift
+//  dporject
+//
+//  Created by THE DUY NGUYEN on 16/5/19.
+//  Copyright © 2019 THE DUY NGUYEN. All rights reserved.
+//
+
+import UIKit
+
+class CommentListCollectionViewCell: UICollectionViewCell {
+    var comment: Comment!
+    
+    func bind(comment: Comment){
+        print(comment)
+    }
+}
