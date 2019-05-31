9fcef157d33bf30256525e5e1d9a567b485b42c7
Dave <duy27295@gmail.com>
2019-05-31 16:17:42 +1000

    add framework

---
+++ dporject/CircleMenu/CircleMenu/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CircleMenu
+//
+//  Created by Alex K. on 27/01/16.
+//  Copyright © 2016 Alex K. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
